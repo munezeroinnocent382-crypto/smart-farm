cdcbb63bc3a79bee8d41707abac01d8050cb97ad
Innocent <munezeroinnocent382@gmail.com>
2026-08-28 17:25:19 +0200

    Add functionality to open and close navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,30 @@
 const open = document.getElementById("open");
+const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
+
+let isOpened = false;
+
+close.style.display = "none";
 
 open.addEventListener("click", function () {
   nav.style.right = 0;
+  isOpened = true;
+  buttonDisplay(isOpened);
 });
+
+close.addEventListener("click", function () {
+  nav.style.right = "-300px";
+  isOpened = false;
+  buttonDisplay(isOpened);
+});
+
+function buttonDisplay(arg) {
+  if (arg === true) {
+    open.style.display = "none";
+
+    close.style.display = "block";
+  } else {
+    open.style.display = "block";
+    close.style.display = "none";
+  }
+}
